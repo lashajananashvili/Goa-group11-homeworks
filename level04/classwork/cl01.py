43bb9c71c6d7c6d7bd74b9ed60e280a8a2196802
lashajananashvili <lashajananashvili13@gmail.com>
2024-05-28 19:15:39 +0400

    commit

---
+++ level04/classwork/cl01.py
@@ -0,0 +1,17 @@
+#task01
+integeri=14
+floati=12.56
+#aq dagviwers float tipis cvladi radgan erts aris atwiladi
+# da meore aris naturaluri ricxvi rodesac naturalu ricxvs mivumatebt atwilads 
+#mivirebt atwilads anu float tipis cvlads
+print(float(integeri)+floati)
+#task02
+bool1=True
+bool2=False
+print(bool1+bool2)
+
+#task03
+num1=20
+num2=24
+print(str(num1)+str(num2))
+
---
+++ level04/classwork/clw03.py
@@ -0,0 +1,7 @@
+#resing is when you have one varieble and you gave that variable two meanings and compiuter will
+# compiuter will give the varieble last meaning you write for example:
+
+nd=10
+nd=15
+print(nd)
+#in this situation if you print and it will print 15 on terminal becouse its the last one you have written
