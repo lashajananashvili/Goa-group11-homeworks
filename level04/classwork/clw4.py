407b6eeac55d5dfc297a8572aef21eb984550c22
lashajananashvili <lashajananashvili13@gmail.com>
2024-05-28 21:13:12 +0400

    commit

---
+++ level04/classwork/clw4.py
@@ -0,0 +1,6 @@
+#task1
+#resing is used to do matematic
+ad=saba
+ba=ba
+gd=ad-ba
+print(gd)
---
+++ level04/homework/hw.4.py
@@ -0,0 +1,8 @@
+dog_name1='rick'
+dog_name2='jonh'
+dog_name3='max'
+
+print('first dog name is'+" "+dog_name1+' '+'secend dog name is '+dog_name2+'.')
+
+
+
