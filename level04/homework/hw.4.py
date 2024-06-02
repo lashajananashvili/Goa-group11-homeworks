4c9ea99fa18a4a60062e3cc90cf37be8b941d44c
lashajananashvili <lashajananashvili13@gmail.com>
2024-06-02 11:05:13 +0400

    commit

--- level04/homework/hw.4.py
+++ level04/homework/hw.4.py
@@ -1,8 +1,34 @@
+#firstly we craeted str varibles which are dog names
 dog_name1='rick'
 dog_name2='jonh'
 dog_name3='max'
-
+#then we told to compiuter to print out dog names with some teext which tells us which one is first dog name and which one is second dogs name
 print('first dog name is'+" "+dog_name1+' '+'secend dog name is '+dog_name2+'.')
+
+
+
+
+#n1
+
+country="Georgia"
+print(f"hello you are fron{country}")
+
+#N2
+region='qartli'
+print(f'hello you ware born in {region}')
+
+#N3
+time=2
+print(f"hello its {2} oclock")
+
+#4
+
+day='tuesday'
+print(f"hello its {day} have nice day")
+
+#N5
+month='june'
+print(f"hello your birthday is in {month}")
 
 
 
