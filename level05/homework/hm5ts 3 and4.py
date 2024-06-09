301210fd79a901ef9665062d21f25ada552f2313
lashajananashvili <lashajananashvili13@gmail.com>
2024-06-09 15:16:55 +0400

    commit

---
+++ level05/homework/hm5ts 3 and4.py
@@ -0,0 +1,7 @@
+name=input('whats your name? ')
+last_name=input('whats your laastname? ')
+age=int(input('how old are you? '))
+password=input('whats your password? ')
+
+print('hello' + ' ' + name + ' ' + last_name + ' ' + 'you are '+ str(age) +' years old'+'your password is '+password )
+print(f'hello {name} {last_name} you are {age} years old your password is {password}' ed3)
---
+++ level05/homework/hw.5 ts2.py
@@ -0,0 +1,12 @@
+#we declare 3 variubles
+num1=3
+num2=4
+num3=12
+#we did all of the mathematical operators we know
+print(num1+num2)
+print(num3-num1)
+print(num2*num3)
+print(num3/num2)
+
+
+
