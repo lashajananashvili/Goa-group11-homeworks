b3d89ff5dd9133f031108e6484e353d628736087
lashajananashvili <lashajananashvili13@gmail.com>
2024-06-10 13:53:08 +0400

    commit

--- level06/homework/hm5ts 3 and4.py
+++ level06/homework/hm5ts 3 and4.py
@@ -1,7 +1,8 @@
+
 name=input('whats your name? ')
 last_name=input('whats your laastname? ')
 age=int(input('how old are you? '))
 password=input('whats your password? ')
 
 print('hello' + ' ' + name + ' ' + last_name + ' ' + 'you are '+ str(age) +' years old'+'your password is '+password )
-print(f'hello {name} {last_name} you are {age} years old your password is {password}' ed3)
+print(f'hello {name} {last_name} you are {age} years old your password is {password}')
