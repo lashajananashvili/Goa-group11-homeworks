e4083c88d8f538797f577dcdce14b13ba344b740
lashajananashvili <lashajananashvili13@gmail.com>
2024-06-11 20:27:40 +0400

    commit

--- level08/homework/task4.py
+++ level08/homework/task4.py
@@ -11,8 +11,8 @@ else:
 #the or function is a logical operator used to combine two or more conditional statements. 
 #It evaluates the expressions on its left and right and returns True if at least one of them is true; otherwise, it returns False. :
 
-point=int(input(""))
-if points>=6 or points==5 :
+point=int(input("how much points do you have? "))
+if point>=6 or point==5 :
     print("congratulation you can enter next grade ")
 else:
     print("i am sorry but you cant go to next grade ")
