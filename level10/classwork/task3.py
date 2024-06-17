8229d5ed2da4f164623754fff8b114cbf4359aef
lashajananashvili <lashajananashvili13@gmail.com>
2024-06-17 19:47:36 +0400

    commit

---
+++ level10/classwork/task3.py
@@ -0,0 +1,4 @@
+
+#3) for loopის გამოყენებით გამოიტანეთ Hello world 100ჯერ
+for i in range(100):
+    print('hello world')
---
+++ level10/homework/task4.py
@@ -0,0 +1,9 @@
+#4) while loopის გამოყენებით გამოიტანეთ 1დან 10მდე რიცხვების ჯამი.
+
+i=0
+answer=0
+while i<10:
+    i+=1
+    answer=i+answer
+
+print(answer)
