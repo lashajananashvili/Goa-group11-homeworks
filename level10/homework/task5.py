3eb979c485d4a90a98c7e77a1edd0229929da5e3
lashajananashvili <lashajananashvili13@gmail.com>
2024-06-19 13:43:11 +0400

    commit

--- level10/homework/task5.py
+++ level10/homework/task5.py
@@ -1,51 +1,51 @@
 #5) ითამაშეთ ამ ორი loopით, გააკეთეთ 5 for loop მაგალითი და 5 while loop მაგალითი (ისეთი რაც აქამდე არ გაგიკეთებიათ).
 
 #for N1
-#number=int(input("pick a number   "))
+number=int(input("pick a number   "))
 
-#for i in range(2,number,2):
-#    print(i)
+for i in range(2,number,2):
+    print(i)
 
-#print("here are all of the even numbers from 1 to your chosen number")
+print("here are all of the even numbers from 1 to your chosen number")
 
 
 #for N2
 
-#odd=int(input("pick a number  "))
+odd=int(input("pick a number  "))
 
-#for i in range(1,odd,2):
-#    print(i)
-#print("here are all of the odd numbers from 1 to your chosen number")
+for i in range(1,odd,2):
+    print(i)
+print("here are all of the odd numbers from 1 to your chosen number")
 
 #for N3
 
-#import time
-#for second in range(10,0,-1):
-#    print(second)
-#    time.sleep(1)
-#print('happy birthday')
+import time
+for second in range(10,0,-1):
+    print(second)
+    time.sleep(1)
+print('happy birthday')
 
 #for N4
 
-#import time
-#for second in range(10,0,-1):
-#    print(second)
-#    time.sleep(1)
-#print('happy new year!!!')
+
+for second in range(10,0,-1):
+    print(second)
+    time.sleep(1)
+print('happy new year!!!')
 
 #for N5
-#name=input("what's your name?")
-#for i in name:
-#    print(i)
-#print("here are all the numbers that your name has")
+name=input("what's your name?")
+for i in name:
+    print(i)
+print("here are all the numbers that your name has")
 
 
 #while N1
 
-#while True:
-#    username =input('please enter your username: ')
-#    if username !='':
-#      break
+while True:
+    username =input('please enter your username: ')
+    if username !='':
+      break
 
 #while N2
 i=0
