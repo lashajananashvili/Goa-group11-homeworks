e9557b02541503ef679cc436c85f53a4c61c060a
lashajananashvili <lashajananashvili13@gmail.com>
2024-06-23 10:18:38 +0400

    commit

--- level12/homework/task2.py
+++ level12/homework/task2.py
@@ -1,4 +1,9 @@
+
+#2) ხელახლა გააკეთეთ კლასში გაკეთებული dice roll თამაში. აიღეთ რაიმე რიცხვი 1დან 6ის ჩათვლით რაც იქნება სწორი პასუხი. 
+# აიღეთ ასევე guess რომელსაც მომხმარებელი შემოიტანს. თუ სწორად გამოიცნობს დაუწერეთ "It's correct", თუ არასწორად "It's incorrect".
+
 guess=int(input("choose the number from 1 to 6:  "))
+
 if guess==4:
     print("its correct")
 else:
--- level12/homework/task7.py
+++ level12/homework/task7.py
@@ -1,5 +1,8 @@
 
+#მომხმარებელს შემოატანინეთ რიცხვი და უთხარით ლუწია თუ კენტი. (hints: 10 % 2 = 0;5 % 2 = 1)'''
+
 num=int(input("pick a number:  "))
+
 if num%2==0:
     print("your choosen number is even")
 else:
