166a1afba4f16af334d604dce0fc07984f41ab7f
lashajananashvili <lashajananashvili13@gmail.com>
2024-08-13 22:51:03 +0400

    commit

---
+++ level26/homework/task3.py
@@ -0,0 +1,4 @@
+#    Python tuples are a type of data structure that is very similar to lists. 
+#    The main difference between the two is that tuples are immutable, meaning 
+#    they cannot be changed once they are created. This makes them ideal for 
+#    storing data that should not be modified, such as database records.
